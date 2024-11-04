c021e060879dbd907beaad3368847e5aaedd389b
MilosJovic23 <milos.mj992@gmail.com>
2024-11-04 10:15:28 +0100

    * homework

---
+++ Domaci.php
@@ -0,0 +1,54 @@
+<?php
+
+
+
+
+
+
+
+
+
+    function izracunajDostavu ( $cena , $grad ){
+
+        $dostava = [
+            "subotica" => 220,
+            "pancevo" => 10,
+            "sarajevo" => 292,
+            "split" => 799
+        ];
+
+        $grad = strtolower($grad);
+        $proveraDostave = array_key_exists( $grad , $dostava );
+
+        if( !$proveraDostave ){
+            die("Za sad ne dostavljamo na vasu adresu :(");
+        }
+
+        foreach ($dostava as $gradZaDostavu => $udaljenostOdBg){
+            if ( $gradZaDostavu == $grad ){
+                $udaljenost = $udaljenostOdBg;
+            }
+
+        }
+
+        if( $udaljenost < 100 ) {
+            $cenaDostave = 200;
+        } else if ( $udaljenost < 200 ) {
+            $cenaDostave = 350;
+        } else {
+            $cenaDostave = 500;
+        }
+
+
+    return $cenaDostave;
+
+    }
+
+    $cenaDostave = izracunajDostavu( 2000 , "SpLiT" );
+
+    echo  $cenaDostave;
+
+
+
+
+?>
